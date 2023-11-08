b2d3f0814166f0e38da75deaa4e08e3e2ce607f3
sivasubramanics <siva.subramani975@gmail.com>
2023-11-08 12:34:19 +0100

    bugfix

--- GAPITv3_GD.R
+++ GAPITv3_GD.R
@@ -6,19 +6,22 @@ library(GAPIT)
 
 args<-commandArgs(TRUE)
 
-CWD <- args[1]
-PHE <- args[2]
-GD <- args[3]
-GM <- args[4]
-MODEL <- args[5]
+CWD <- args[1] # Ouput directory
+PHE <- args[2] # Phenotype file
+GD <- args[3] # Genotype data
+GM <- args[4] # Genotype map
+MODEL <- args[5] # Model
 
-dir.create(CWD)
-setwd(CWD)
-
+# load genotype data and phenotype data
 myGD=read.table(GD,header=T,sep="\t")
 myGM=read.table(GM,header=T,sep="\t")
 myY=read.table(PHE,header=T,sep="\t")
 
+# create a directory for GAPIT output
+dir.create(CWD)
+setwd(CWD)
+
+# run GAPIT
 myGAPIT=GAPIT(
   Y=myY, #fist column is ID
   GD=myGD,
