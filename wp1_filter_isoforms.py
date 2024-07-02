0a8dd6f8bdbeebf5b692a36c9a0f934caab88709
selva001 <siva.selvanayagam@wur.nl>
2024-07-02 21:25:24 +0200

    Added more stuffs

--- wp1_filter_isoforms.py
+++ wp1_filter_isoforms.py
@@ -19,7 +19,8 @@ class Fasta:
 
 
 class Aln:
-    def __init__(self, qseqid, sseqid, pident, length, mismatch, gapopen, qstart, qend, sstart, send, evalue, bitscore, qcovhsp, scovhsp, qlen, slen):
+    def __init__(self, qseqid, sseqid, pident, length, mismatch, gapopen, qstart, qend, sstart, send, evalue, bitscore,
+                 qcovhsp, scovhsp, qlen, slen):
         self.qseqid = qseqid
         self.sseqid = sseqid
         self.pident = pident
@@ -38,9 +39,16 @@ class Aln:
         self.slen = slen
 
     def is_complete(self, min_pident, min_scovhsp):
-        if self.pident >= min_pident and self.scovhsp >= min_scovhsp:
+        if float(self.pident) >= min_pident and float(self.scovhsp) >= min_scovhsp:
             return True
         return False
+
+
+def fold_seq(seq, n=60):
+    """
+    Fold the sequence into n characters
+    """
+    return '\n'.join([seq[i:i + n] for i in range(0, len(seq), n)])
 
 
 def parse_fasta(in_fa_file):
@@ -59,7 +67,7 @@ def parse_fasta(in_fa_file):
                 id = line.strip().split()[0][1:]
                 desc = " ".join(line.strip().split()[1:])
             else:
-                seq += line.strip()
+                seq += line.strip().replace("*", "")
         if seq:
             yield Fasta(id, seq, desc)
 
@@ -77,8 +85,6 @@ def parse_uniref_desc(desc):
     parts = desc.split()
     parsed_data = {}
 
-    print(desc)
-    print(parts)
     desc_parts = []
     for part in parts:
         if part.startswith("n="):
@@ -99,6 +105,7 @@ def parse_uniref_desc(desc):
 def main():
     parser = argparse.ArgumentParser(description='Filter isoforms based on the uniref annotation')
     parser.add_argument('-i', '--input', help='Input isoforms fasta file', required=True)
+    parser.add_argument('-a', '--proteins', help='Proteins fasta file', required=True)
     parser.add_argument('-o', '--output', help='Output prefix', required=True)
     parser.add_argument('-m', '--gene_to_tr_map', help='Gene to transcript map file', required=True)
     parser.add_argument('-d', '--diamond', help='Diamond output file', required=True)
@@ -107,7 +114,8 @@ def main():
     parser.add_argument('-c', '--coverage', help='Minimum coverage', required=False, default=80)
     args = parser.parse_args()
 
-    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s')
+    # Set up the logging in the format to print the time: [2024-02-02 12:12:12] message
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s')
 
     logging.info(f'Reading the uniref90 fasta file {args.uniref}')
     uniref = defaultdict()
@@ -125,6 +133,16 @@ def main():
                 gene_to_tr[gene] = []
             gene_to_tr[gene].append(tr)
 
+    logging.info(f'Reading the proteins fasta file {args.proteins}')
+    proteins = defaultdict()
+    for fasta in parse_fasta(args.proteins):
+        proteins[fasta.id] = fasta
+
+    logging.info(f'Reading the isoforms fasta file {args.input}')
+    isoforms = defaultdict()
+    for fasta in parse_fasta(args.input):
+        isoforms[fasta.id] = fasta
+
     logging.info(f'Reading the diamond output file {args.diamond}')
     aln_dict = defaultdict()
     with open(args.diamond, 'r') as f:
@@ -135,25 +153,58 @@ def main():
             if aln.qseqid not in aln_dict:
                 aln_dict[aln.qseqid] = []
             else:
-                logging.error(f"Duplicate alignment found for {aln.qseqid}. Make sure you provide only the best hits in the diamond output file")
+                logging.error(
+                    f"Duplicate alignment found for {aln.qseqid}. Make sure you provide only the best hits in the diamond output file")
             aln_dict[aln.qseqid].append(aln)
             aln_dict[aln.qseqid].append(uniref[aln.sseqid])
 
     logging.info(f'Filtering the alignments')
-    with open(args.output + ".classified.tsv", 'w') as f:
+    with open(args.output + ".classified.tsv", 'w') as raw, open(args.output + ".classified.filtered.tsv", 'w') as flt:
         for gene_id in gene_to_tr:
+            scores = defaultdict()
             for tr_id in gene_to_tr[gene_id]:
+                if tr_id not in proteins:
+                    raw.write(f"{gene_id}\t{tr_id}\tNoProtein\n")
+                    continue
                 if tr_id not in aln_dict:
-                    f.write(f"{gene_id}\t{tr_id}\tUnmapped\n")
+                    raw.write(f"{gene_id}\t{tr_id}\tUnmapped\n")
                     continue
                 aln = aln_dict[tr_id][0]
                 ann = aln_dict[tr_id][1]
-                f.write(
-                    f"{gene_id}\t{tr_id}\t{aln.sseqid}\t{aln.pident}\t{aln.qcovhsp}\t{aln.scovhsp}\t{ann['Tax']}\t{ann['desc']}")
+                scores[tr_id] = aln.bitscore
+                raw.write(
+                    f"{gene_id}\t{tr_id}\t{aln.sseqid}\t{aln.pident}\t{aln.qcovhsp}\t{aln.scovhsp}\t{aln.bitscore}\t{ann['Tax']}\t{ann['desc']}\t{len(isoforms[tr_id].seq)}")
                 if aln.is_complete(args.percent, args.coverage):
-                    f.write("\tComplete\n")
+                    raw.write("\tComplete\n")
                 else:
-                    f.write("\tIncomplete\n")
+                    raw.write("\tIncomplete\n")
+            if scores:
+                tr_id = max(scores, key=scores.get)
+                # check if other transcripts have the same score
+                if list(scores.values()).count(scores[tr_id]) > 1:
+                    # get the list of transcripts with the same score
+                    tr_ids = [tr for tr in scores if scores[tr] == scores[tr_id]]
+                    # get the longest transcript
+                    tr_id = max(tr_ids, key=lambda x: len(isoforms[x].seq))
+                aln = aln_dict[tr_id][0]
+                ann = aln_dict[tr_id][1]
+                flt.write(
+                    f"{gene_id}\t{tr_id}\t{aln.sseqid}\t{aln.pident}\t{aln.qcovhsp}\t{aln.scovhsp}\t{aln.bitscore}\t{ann['Tax']}\t{ann['desc']}\t{len(isoforms[tr_id].seq)}")
+                if aln.is_complete(args.percent, args.coverage):
+                    flt.write("\tComplete\n")
+                else:
+                    flt.write("\tIncomplete\n")
+
+    logging.info(f'Writing the filtered isoforms')
+    with (open(f"{args.output}.fasta", 'w') as tr_fa,
+          open(f"{args.output}.gene_to_tr_map", 'w') as map,
+          open(f"{args.output}.faa", 'w') as faa,
+            open(f"{args.output}.classified.filtered.tsv", 'r') as f):
+        for line in f:
+            gene_id, tr_id, uniref_id, pident, qcovhsp, scovhsp, bitscore, tax, desc, length, status = line.strip().split('\t')
+            tr_fa.write(f">{gene_id}\n{fold_seq(isoforms[tr_id].seq)}\n")
+            faa.write(f">{gene_id}\n{fold_seq(proteins[tr_id].seq)}\n")
+            map.write(f"{gene_id}\t{gene_id}\n")
 
 
 if __name__ == "__main__":
